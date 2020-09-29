450263a98fe6f3b98d2bc98707f5cb6c8c739eab
Ashutosh-Aryal <aryala@usc.edu>
2020-09-29 16:04:12 -0700

    Made game infinite but only one at a time

--- JumpFish/Assets/JumpFishBehavior.cs
+++ JumpFish/Assets/JumpFishBehavior.cs
@@ -8,6 +8,9 @@ public class JumpFishBehavior : MonoBehaviour
     private const KeyCode JUMP_UP_KEY = KeyCode.W;
     private const KeyCode JUMP_DOWN_KEY = KeyCode.S;
 
+    private const string ADD_POINT_TRIGGER_TAG = "ResetNumJumps";
+    private const string WALL_TAG = "Wall";
+
     private const float JUMP_MAX_THRESHOLD_INCREMENT = 0.25f;
 
     [SerializeField]
@@ -16,12 +19,38 @@ public class JumpFishBehavior : MonoBehaviour
     [SerializeField]
     private Slider m_Slider;
 
+    private int m_Score = 0;
+
+    public static int s_NumAllowedJumps = 3;
+
     private float m_JumpHoldingTimer = 0.0f;
     private int m_NumRowsToJump = 0;
     private int m_CurrentLocationIndex;
     private bool m_ShouldIncrementJumpTimer = false;
     
     private List<GameObject> m_TeleportLocations = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(ADD_POINT_TRIGGER_TAG))
+        {
+            m_Score++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(WALL_TAG))
+        {
+            m_Score = 0;
+            DisplayEndGameState();
+        }
+    }
+
+    private void DisplayEndGameState()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -58,13 +87,14 @@ public class JumpFishBehavior : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(m_ShouldIncrementJumpTimer) {
-            m_JumpHoldingTimer += Time.fixedDeltaTime;
-        }
 
         float THRESHOLD_TO_INCREMENT_NUM_ROWS_JUMPED = JUMP_MAX_THRESHOLD_INCREMENT * Mathf.Abs(m_NumRowsToJump);
+        
+        if (m_ShouldIncrementJumpTimer) {
+            m_JumpHoldingTimer += Time.fixedDeltaTime;
+            m_Slider.value = THRESHOLD_TO_INCREMENT_NUM_ROWS_JUMPED - JUMP_MAX_THRESHOLD_INCREMENT;
+        }
 
-        m_Slider.value = THRESHOLD_TO_INCREMENT_NUM_ROWS_JUMPED - JUMP_MAX_THRESHOLD_INCREMENT;
         bool shouldIncrementRowsToJump = m_JumpHoldingTimer >= THRESHOLD_TO_INCREMENT_NUM_ROWS_JUMPED;
 
         if(shouldIncrementRowsToJump) {
@@ -82,9 +112,10 @@ public class JumpFishBehavior : MonoBehaviour
         } else if(Input.GetKeyUp(JUMP_UP_KEY) || Input.GetKeyUp(JUMP_DOWN_KEY)) {
             
             int jumpToIndex = m_CurrentLocationIndex + m_NumRowsToJump;
-            m_CurrentLocationIndex = Mathf.Clamp(jumpToIndex, 0, m_TeleportLocations.Count);
+            m_CurrentLocationIndex = Mathf.Clamp(jumpToIndex, 0, m_TeleportLocations.Count - 1);
             gameObject.transform.position = m_TeleportLocations[m_CurrentLocationIndex].transform.position;
             m_JumpHoldingTimer = 0.0f; m_ShouldIncrementJumpTimer = false;
+            m_Slider.value = 0.0f;
         } 
     }
 }
--- JumpFish/Assets/WallMovement.cs
+++ JumpFish/Assets/WallMovement.cs
@@ -4,14 +4,76 @@ using UnityEngine;
 
 public class WallMovement : MonoBehaviour
 {
+    private const float TIME_BETWEEN_SPAWNS = 5.0f;
+    private const float LEFT_X_BOUNDARY = -220.0f;
+    
+    private bool m_ShouldSpawnWall = true;
+
+    private float m_SpawnTimer = 0.0f;
+
+    [SerializeField]
+    private GameObject m_WallsContainer;
+
+    [SerializeField]
+    private Transform m_WallSpawnLocation;
+
+    private List<GameObject> m_Walls = new List<GameObject>();
+
+    private List<GameObject> m_MovingWalls = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
+        int numChildren = m_WallsContainer.transform.childCount;
+
+        for(int x = 0; x < numChildren; x++) {
+            m_Walls.Add(m_WallsContainer.transform.GetChild(x).gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(m_ShouldSpawnWall)
+        {
+            SpawnWall();
+        }
+
+        CheckIfOutOfBounds();
+
+        m_SpawnTimer += Time.deltaTime;
+    }
+
+    private void SpawnWall()
+    {
+        int randIndex = Random.Range(0, m_Walls.Count);
+
+        GameObject spawnedWall = Instantiate(m_Walls[randIndex], m_WallSpawnLocation);
+        spawnedWall.GetComponent<Rigidbody2D>().velocity = new Vector2(-100.0f, 0.0f);
+
+        m_MovingWalls.Add(spawnedWall);
+
+        m_ShouldSpawnWall = false;
+    }
+
+    private void CheckIfOutOfBounds()
+    {
+        List<int> removedIndices = new List<int>();
         
+        for(int x = 0; x < m_MovingWalls.Count; x++)
+        {
+            if(m_MovingWalls[x].transform.position.x <= LEFT_X_BOUNDARY)
+            {
+                removedIndices.Add(x);
+                m_ShouldSpawnWall = true;
+            }
+        }
+
+        foreach(int index in removedIndices)
+        {
+            Destroy(m_MovingWalls[index]);
+            m_MovingWalls.RemoveAt(index);
+        }
+
     }
 }
